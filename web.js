c8355f8bebf737a49dbf703c845c8303dfe6680d
dawood abbaspour <dahstar@ubuntu.(none)>
2013-07-08 01:01:31 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -2,10 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var content=fs.readFileSync("index.html");
+buf = new Buffer(content);
+len=buf.write('\u00bd + \u00bc=\u00be',0);
+respond.send(buf.toString('utf8',0,len));
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
+	response.send('hi');
+    });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
