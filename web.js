c304fbcbc33a121db46d8e37ac94653179695d3b
dawood abbaspour <davood.abbaspour@gmail.com>
2013-07-09 02:33:48 -0700

    updated web.js

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var data = fs.readFileSync('./index.html', 'utf8'); 
 app.get('/', function(request, response) {
-	response.send('hi again' );
+	response.send('data sis'+data);
     });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
