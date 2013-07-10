3f231a45d9204dc652058e74fbc2afa73ba871b5
dawood abbaspour <davood.abbaspour@gmail.com>
2013-07-10 00:52:33 -0700

    updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,48 @@
-var http = require('http'),
-    fs = require('fs');
+ var http = require('http');
+ var fs = require('fs');
+ var path = require('path');
 
+ http.createServer(function (request, response) {
 
-fs.readFile('./index.html', function (err, html) {
-	if (err) {
-	    throw err; 
-	}       
-	http.createServer(function(request, response) {  
-		response.writeHeader(200, {"Content-Type": "text/html"});  
-		response.write(html);  
-		response.end();  
-	    }).listen(8000);
+    console.log('request starting for ');
+    console.log(request);
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    console.log(filePath);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    path.exists(filePath, function(exists) {
+
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
     });
+
+ }).listen(process.env.PORT || 5000);
+
+ console.log('Server running at http://127.0.0.1:5000/');
